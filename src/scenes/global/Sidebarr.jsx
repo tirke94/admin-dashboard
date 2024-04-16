70679b077f7276df9d739961802545360dc5b848
tirke94 <nikola.tijanic94@gmail.com>
2024-04-16 18:46:09 +0200

    added some changes

--- src/scenes/global/Sidebarr.jsx
+++ src/scenes/global/Sidebarr.jsx
@@ -108,7 +108,7 @@ const Sidebarr = () => {
                   fontWeight="bold"
                   sx={{ m: "10px 0 0 0" }}
                 >
-                  Ed Roh
+                  Nikola Tijanic
                 </Typography>
                 <Typography variant="h5" color={colors.greenAccent[500]}>
                   VP Fancy Admin
